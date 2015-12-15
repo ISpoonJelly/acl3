9d2a2fc0e576e1c28ce2b3bc32faba0f46ecf3bb
ISpoonJelly <tiitomoha@gmail.com>
2015-12-15 17:21:44 +0200

    rails commit

--- app/controllers/api/followings_controller.rb
+++ app/controllers/api/followings_controller.rb
@@ -21,11 +21,11 @@ class Api::FollowingsController < Api::BaseController
 
   def destroy
       @following = Following.where(:user_id => current_user, :follower_id => params[:follower])
-      @following.destroy
-  end
-
-protected
-  def following_params
-    params.require(:following).permit(:follower)
+      
+      if @following.destroy
+          render :status => "200", :json => {:status => "success"}.to_json
+      else
+          render :status => "400", :json => {:status => "failure"}.to_json
+      end
   end
 end
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::PostsController < Api::BaseController
-    before_action :authenticate_user!, only: [:create]
+    before_action :authenticate_user!, only: [:create, :update]
     
     def index
         if params[:destination].nil?
@@ -27,4 +27,14 @@ class Api::PostsController < Api::BaseController
     	@post = Post.create(:user => User.first, :body => params[:body], :destination => @destination_user)
     	respond_with @post
 	end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.likes += 1
+        if @post.save
+            render :status => "200", :json => {:status => "success"}.to_json
+        else
+            render :status => "400", :json => {:status => "failure"}.to_json
+        end
+    end
 end
